9787d5b10dc5e6cf145742fdb25562fc0cd41342
Jeffrey Freet <jeffreyfreet@yahoo.com>
2018-06-19 10:00:42 -0400

    Added simple click to move feature

---
+++ RPG Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    private NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+        navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navAgent.destination = hit.point;
+            }
+        }
+	}
+}
