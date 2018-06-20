18f7e348b6b901eeaf1216f99a47ef720454bb8b
Jeffrey Freet <jeffreyfreet@yahoo.com>
2018-06-20 19:01:46 -0400

    Scene work

---
+++ RPG Game/Assets/Scripts/LightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
