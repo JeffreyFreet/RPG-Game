6e6645073d7321240a9e584f7f27a9cf5cb2d0e8
Jeffrey Freet <jeffreyfreet@yahoo.com>
2018-06-21 16:03:52 -0400

    Prison cell gate added

---
+++ RPG Game/Assets/Scripts/Interactables/DungeonGate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DungeonGate : Interactable {
+
+    public override void Interact()
+    {
+        //Obviously need to grab key and check inventory for said key
+        Debug.Log("Opening Gate!");
+        gameObject.SetActive(false);
+    }
+}
